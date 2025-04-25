a97356d808e8d295474224046bfd8b3c1c17a2e7
Lisias T <me@lisias.net>
2025-04-25 09:22:57 -0300

    Decluttering: removing non used symbols from DeviceManager

--- src/StreamDeck/DeviceManager.py
+++ src/StreamDeck/DeviceManager.py
@@ -38,18 +38,6 @@ class DeviceManager:
     StreamDeck devices.
     """
 
-    USB_VID_ELGATO = 0x0fd9
-    USB_PID_STREAMDECK_ORIGINAL = 0x0060
-    USB_PID_STREAMDECK_ORIGINAL_V2 = 0x006d
-    USB_PID_STREAMDECK_MINI = 0x0063
-    USB_PID_STREAMDECK_NEO = 0x009a
-    USB_PID_STREAMDECK_XL = 0x006c
-    USB_PID_STREAMDECK_MK2 = 0x0080
-    USB_PID_STREAMDECK_PEDAL = 0x0086
-    USB_PID_STREAMDECK_PLUS = 0x0084
-    USB_VID_MIRABOX = 0x5548
-    USB_PID_MIRABOX_STREAMDOCK_293S = 0x6670
-
     @staticmethod
     def _get_transport(transport):
         """
